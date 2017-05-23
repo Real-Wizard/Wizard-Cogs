b5075c92de94fdd31a0dda7acefae56e598af627
MasterKnight <neomcplayer@gmail.com>
2017-05-23 08:56:52 +0300

    Create info.js

---
+++ bump/info.js
@@ -0,0 +1,8 @@
+
+{
+    "AUTHOR" : "MasterKnight",
+    "INSTALL_MSG" : "Thanks for installing my bump cog!",
+    "NAME" : "bump",
+    "SHORT" : "Let people be able to bump their servers.",
+    "DESCRIPTION" : ""Let people be able to bump their servers."
+}
