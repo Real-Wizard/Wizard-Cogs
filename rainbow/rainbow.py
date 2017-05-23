ee98d8e488880782b81d6dad205380bb05b09138
MasterKnight <neomcplayer@gmail.com>
2017-05-23 08:51:13 +0300

    Rainbow.py Cog

---
+++ rainbow/rainbow.py
@@ -0,0 +1,31 @@
+import discord
+import time
+from discord.ext import commands
+from random import choice, randint
+import cogs.utils
+import asyncio
+from cogs.utils import checks
+
+__author__ = "MasterKnight"
+class rainbow:
+    """Rainbows the selected role"""
+
+    def __init__(self, bot):
+        self.bot = bot
+	
+    @checks.admin_or_permissions(manage_roles=True)
+    @commands.command(pass_context = True, no_pm=True)
+    async def rainbow(self, ctx, *, role: discord.Role):
+        
+        while True:
+            colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
+            colour = int(colour, 16)
+            await self.bot.edit_role(ctx.message.server, role, colour=discord.Colour(value=colour))
+            await asyncio.sleep(1.0)
+            done = discord.Embed(title="I have succesfly made {} a rainbow role".format(role))
+            await self.bot.say(embed=done)
+
+
+def setup(bot):
+    n = rainbow(bot)
+    bot.add_cog(n)
