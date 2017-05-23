68da5566e80cf3694cbc7c2d7dd1ccff73fa24ea
MasterKnight <neomcplayer@gmail.com>
2017-05-23 09:09:34 +0300

    Update info.js

--- bump/info.js
+++ bump/info.js
@@ -3,6 +3,6 @@
     "AUTHOR" : "MasterKnight",
     "INSTALL_MSG" : "Thanks for installing my bump cog!",
     "NAME" : "bump",
-    "SHORT" : "Let people be able to bump their servers.",
-    "DESCRIPTION" : ""Let people be able to bump their servers."
+    "SHORT" : "Let people be able to bump their servers,You Have To Enter The Channel Id In The Code",
+    "DESCRIPTION" : "Let people be able to bump their servers,You Have To Enter The Channel Id In The Code"
 }
