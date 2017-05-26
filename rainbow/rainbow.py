e9877333513516d9c52d52c3cbf49b13f85c78b6
MasterKnight <neomcplayer@gmail.com>
2017-05-26 08:39:01 +0300

    Update rainbow.py

--- rainbow/rainbow.py
+++ rainbow/rainbow.py
@@ -15,15 +15,23 @@ class rainbow:
 	
 	
 	
+
     @checks.admin_or_permissions(manage_roles=True)
     @commands.command(pass_context = True, no_pm=True)
-    async def rainbow(self, ctx, *, role: discord.Role):
-        
+    async def rainbow(self, ctx, interval:float, *, role):
+        roleObj = discord.utils.find(lambda r: r.name == role, ctx.message.server.roles)
+        if not roleObj:
+            no = discord.Embed(title="{} is not a valid role".format(role))
+            await self.bot.say(embed=no)
+            return
+        if interval < 120:
+            interval = 120
         while True:
             colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
             colour = int(colour, 16)
-            await self.bot.edit_role(ctx.message.server, role, colour=discord.Colour(value=colour))
-            await asyncio.sleep(200.0)
+            await self.bot.edit_role(ctx.message.server, roleObj, colour=discord.Colour(value=colour))
+            await asyncio.sleep(interval)
+
 	
 	
 	
