d89ab7787366ff0e97f9caef7dc5c88dbd41a326
MasterKnight <neomcplayer@gmail.com>
2017-05-26 12:43:51 +0300

    Still In Progress

--- spam/spam.py
+++ spam/spam.py
@@ -3,3 +3,9 @@ from discord.ext import commands
 
 class Spam:
 	"""Spam :D"""
+	def __init__(self, bot):
+		self.bot = bot
+		
+	@commands.command()
+	async def spam(self, ctx, user: discord.Member, spamtext, number: int):
+		"""Spam The User hehe"""	
