5fc2c9e9db5b12284b3e424f093bf18cf87bdeac
MasterKnight <neomcplayer@gmail.com>
2017-05-27 08:55:30 +0300

    Finished .py File

--- PM/pm.py
+++ PM/pm.py
@@ -4,3 +4,23 @@ from discord.ext import commands
 class PM:
 	"""PM People Using The Bot"""
 	def __init__(self, bot):
+		self.bot = bot
+
+		
+    @commands.command()
+    @checks.is_owner()
+    async def pm(self, user_id: str, *, msg: str):
+	"""Pm a User Using The Bot"""
+        user = await self.bot.get_user_info(user_id)
+        try:
+            await self.bot.send_message(user, content)
+        except:
+            await self.bot.say('Could not DM This User ( ' + user_id + ')')
+        else:
+            await self.bot.say('DM successfully sent.')
+# IDEA FROM DANNY
+def setup(bot):
+	n = PM(bot)
+	bot.add_cog(n)
+
+
